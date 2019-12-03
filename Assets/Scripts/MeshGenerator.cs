8afd218190c0fba0136f4d082a5483159209ef72
Bala Shanmugam <balashanmugam.1996@gmail.com>
2019-12-03 12:41:36 -0800

    WIP: Working on player input [Single player]

---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+
+namespace LineAR {
+    public class MeshGenerator : MonoBehaviour {
+        [SerializeField] private GameObject unitCircle;
+
+        [SerializeField] private GameObject last;
+
+        [SerializeField] private float TIMESTEP = 0.01f;
+        [SerializeField] private float timer = 0.01f;
+
+        private void Start() {
+            last = this.gameObject;
+            Spawn();
+
+        }
+
+        private void Spawn() {
+            //spawn a small cylinder
+            var obj = Instantiate(unitCircle, last.transform.position + (transform.forward * 0.05f), Quaternion.identity, gameObject.transform);
+            if (obj != null) {
+                last = obj;
+            }
+        }
+
+        private void FixedUpdate() {
+            // keep spawning
+            timer -= Time.fixedDeltaTime;
+            if (timer <= 0) {
+                Spawn();
+                timer = TIMESTEP;
+            }
+        }
+    }
+}
---
+++ Assets/Utils.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace LineAR {
+    public static class Utils {
+        
+        // Bala Bresnham's circle drawing algorithm 
+        public static Vector3[] CreateCirclePoints() {
+            // Step 1
+            // Set initial conditions
+            Vector3 point;
+            float _d = 0;
+            float _radius = 1;
+            point.x = 0;
+            point.y = _radius;
+            _d = 3 - (2 * _radius);
+
+            // step 2
+            
+
+            return null;
+        }
+        
+        
+    }
+}

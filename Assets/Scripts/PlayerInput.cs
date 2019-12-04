7dcaa9cc8a9fc8a689c192faad58398668c9c11c
Bala Shanmugam <balashanmugam.1996@gmail.com>
2019-12-03 20:11:40 -0800

    add mobile input

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -3,11 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using LineAR;
 using UnityEngine;
+
 /// <summary>
 /// Input manager class
 /// </summary>
-public class PlayerInput : MonoBehaviour
-{
+public class PlayerInput : MonoBehaviour {
     // Should have an instance of the player.
     [SerializeField] private MeshGenerator player;
 
@@ -22,13 +22,30 @@ public class PlayerInput : MonoBehaviour
     private void Start() {
         player = GetComponent<MeshGenerator>();
     }
-    
+
     private void Update() {
         if (Input.GetButtonDown("Jump")) {
             player.StartGrow = !player.StartGrow;
         }
-        // control input.
-        horizontal = Input.GetAxis("Horizontal");
 
+        // Platform specific input.
+#if UNITY_EDITOR_OSX || UNITY_EDITOR || UNITY_EDITOR_64
+        horizontal = Input.GetAxis("Horizontal");
+#endif
+
+        // control input for mobile.
+#if UNITY_ANDROID
+        if (Input.touchCount > 0) {
+            Touch t = Input.GetTouch(0);
+            if (t.phase == TouchPhase.Began || t.phase == TouchPhase.Stationary || t.phase != TouchPhase.Ended) {
+                if (t.position.x < Screen.width / 2) {
+                    horizontal = -1;
+                }
+                else if (t.position.x > Screen.width / 2) {
+                    horizontal = 1;
+                }
+            }
+        }
+#endif
     }
 }

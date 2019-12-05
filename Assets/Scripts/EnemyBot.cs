b4f13341bf6f5eeac122a873809ee4c0b5559d61
Bala Shanmugam <balashanmugam.1996@gmail.com>
2019-12-04 18:10:18 -0800

    Add singleton. WIP: Bots

---
+++ Assets/Scripts/EnemyBot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBot : MonoBehaviour
+{
+    
+    // List of spawn positions
+    
+    // List of target for each spawn location
+    
+    
+}
--- Assets/Scripts/MeshCollision.cs
+++ Assets/Scripts/MeshCollision.cs
@@ -3,14 +3,7 @@ using UnityEngine;
 using Object = System.Object;
 
 public class MeshCollision : MonoBehaviour {
-
-    //[SerializeField] private GameObject rbCircle;
-
-    private void OnEnable() {
-
-        //rbCircle = this.transform.Find("RigidbodyCircle(Clone)").transform.Find("Cylinder").gameObject;
-    }
-
+    
     private void OnTriggerEnter(Collider other) {
         Debug.Log(other.gameObject.name);
     }
